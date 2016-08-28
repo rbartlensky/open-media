7603397f36a41ebe2e6724e83f88637d814e240a
Gabriela Alexandra Moldovan <gabi_250@live.com>
2016-08-29 01:56:53 +0300

    Add mixer methods.

--- lib/player/mixer.py
+++ lib/player/mixer.py
@@ -1,12 +1,16 @@
 from pygame import mixer
 from track import Track
 
+class InvalidVolumeError(Exception):
+    pass
+
 class Mixer(object):
 
     def __init__(self, song_list):
         self.song_list = [Track(song) for song in song_list]
+        self.song_list.reverse()
         mixer.init()
-        self.current_song = song_list[0]
+        self.current_song = self._get_next_song()
         self.is_paused = False
 
     def play(self):
@@ -24,3 +28,17 @@ class Mixer(object):
 
     def stop(self):
         mixer.music.stop()
+
+    def set_volume(volume):
+        if volume >= 0.0 and volume <= 1.0:
+            mixer.set_volume(volume)
+        else:
+            raise InvalidVolumeError(volume)
+
+    def play_next():
+        self.current_song = self._get_next_song()
+
+    def _get_next_song():
+        old = self.song_list.pop()
+        self.song_list.push(old)
+        return old
