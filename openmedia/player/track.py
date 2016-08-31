6c424fbe82428da04cc0b8be9c58d14b2318c883
Robert Bartlensky <bartlensky.robert@gmail.com>
2016-08-31 03:16:43 +0100

    Add exception handling.

--- openmedia/player/track.py
+++ openmedia/player/track.py
@@ -4,10 +4,14 @@ class Track(object):
 
     def __init__(self, file_path):
         self.__file_path = file_path
-        self.track_file = open(file_path)
-        self.duration = self.track_file.seconds_length()
-        self.meta_data = self.track_file.get_metadata()
-
+        try:
+            self.track_file = open(file_path)
+            self.duration = self.track_file.seconds_length()
+            self.meta_data = self.track_file.get_metadata()
+        except Exception:
+            self.meta_data = None
+            self.duration = 0
+            
     def get_path(self):
         return self.__file_path
 
