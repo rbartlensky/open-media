6e59526e9c3e67b022feca4ad03397953b9db56b
Robert Bartlensky <bartlensky.robert@gmail.com>
2016-09-01 03:26:55 +0100

    Add exception handling and Metadata class.

--- openmedia/player/track.py
+++ openmedia/player/track.py
@@ -1,4 +1,5 @@
-from audiotools import open, Filename
+from audiotools import open, Filename, UnsupportedFile
+import mutagen
 
 class Track(object):
 
@@ -6,11 +7,25 @@ class Track(object):
         self.__file_path = file_path
         try:
             self.track_file = open(file_path)
-            self.duration = self.track_file.seconds_length()
-            self.meta_data = self.track_file.get_metadata()
-        except Exception:
-            self.meta_data = None
-            self.duration = 0
-
+            self.duration = float(self.track_file.seconds_length())
+            self.metadata = self.track_file.get_metadata()
+        except UnsupportedFile:
+            if file_path.endswith(".mp3"):
+                from mutagen.mp3 import MP3, MPEGInfo
+                from mutagen import File
+                self.track_file = File(file_path)
+                self.metadata = Metadata(self.track_file)
+                self.duration = self.track_file.info.length
+            else:
+                self.duration = 0
+                self.metadata = None
+        
     def get_path(self):
         return self.__file_path
+
+class Metadata(object):
+
+    def __init__(self, mutFile):
+        self.artist_name = mutFile["TPE1"]
+        self.album_name = mutFile["TALB"]
+        self.track_name = mutFile["TIT2"]
