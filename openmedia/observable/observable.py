07d3b6f8de97e3b114ffe6132f6e6a7110ba2778
Gabriela Alexandra Moldovan <gabi_250@live.com>
2016-10-07 00:07:22 +0100

    Create observable package.

---
+++ openmedia/observable/observable.py
@@ -0,0 +1,26 @@
+class Observable(object):
+
+    def __init__(self):
+        self._observers = []
+
+    def has_observer(self, observer):
+        for obs in self._observers:
+            if obs == observer:
+                return False
+        return True
+
+    def add_observer(self, observer):
+        if not self.has_observer(observer):
+            self._observers.append(observer)
+
+    def notify_observers(self, info):
+        for obs in selfobservers:
+            obs.update(info)
+
+class Observer(object):
+
+    def __init__(self, observable):
+        self.observable = observable
+
+    def update(self, info):
+        pass
