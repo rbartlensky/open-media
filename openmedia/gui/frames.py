ebceb1f9d5de4e44990b0ca9b466b244a8db5265
Gabriela Alexandra Moldovan <gabi_250@live.com>
2016-10-12 11:51:54 +0100

    Create properties for Track and Metadata.

--- openmedia/gui/frames.py
+++ openmedia/gui/frames.py
@@ -37,7 +37,89 @@ class PlayerFrame(Gtk.Window, Observer):
 
         self.add(grid)
 
-    def update(self, event_type):
+        self.play_next_button = Gtk.Button(label=">>")
+        self.play_next_button.connect("clicked", self._play_next)
+        self.play_next_button.set_vexpand(False)
+
+        self.volume_button = Gtk.VolumeButton()
+        self.volume_button.set_value(0.5)
+        self.volume_button.connect("value-changed", self._volume)
+        self.volume_button.x_align = 1.0
+        self.volume_button.set_vexpand(False)
+
+        self.playlist_button = Gtk.ToggleButton(label="=")
+        self.playlist_button.connect("clicked", self._toggle_playlist)
+        self.playlist_button.set_vexpand(False)
+
+        self.buttonsBox = Gtk.HBox(False)
+        self.buttonsBox.set_spacing(5)
+        self.buttonsBox.pack_start(self.play_button, False, False, 0)
+        self.buttonsBox.pack_start(self.stop_button, False, False, 0)
+        self.buttonsBox.pack_start(self.play_next_button, False, False, 0)
+        self.volumeBox = Gtk.HBox()
+        self.volumeBox.pack_end(self.volume_button, False, True, 0)
+        self.buttonsBox.pack_start(self.playlist_button, False, False, 0)
+        self.buttonsBox.pack_end(self.volumeBox, True, True, 0)
+        self.buttonsBox.set_vexpand(False)
+
+    def _create_playlist(self):
+        self.model = Gio.ListStore.new(ModelItem)
+        self.playlist = Gtk.ListBox()
+        self.playlist.bind_model(self.model, self._create_list_item, None)
+        self.playlist.connect("row_activated", self._play_item)
+        for track in mixer.track_list:
+            item = ModelItem(track.name, track.duration)
+            self.model.append(item)
+        self.playlist.select_row(self.playlist.get_row_at_index(0))
+        self.add_track = Gtk.Button(label="+")
+        self.add_track.connect("clicked", self._add_track)
+
+        self.playlistBox = Gtk.VBox()
+        self.playlistBox.set_spacing(5)
+        self.playlistBox.pack_start(self.playlist, False, False, 0)
+        self.playlistBox.pack_start(self.add_track, False, False, 0)
+        self.playlistBox.set_vexpand(False)
+
+    def _create_list_item(self, item, data):
+        hbox = Gtk.HBox(5)
+        title_label = Gtk.Label(item.title)
+        duration_label = Gtk.Label(hms_format(item.duration))
+        title_label.show()
+        duration_label.show()
+        hbox.pack_start(title_label, False, False, 0)
+        hbox.pack_start(duration_label, False, False, 0)
+        return hbox
+
+    def _play_item(self, list_box, row):
+        mixer.play(mixer.track_list[row.get_index()].file_path)
+
+    def _add_track(self, widget):
+        dialog = Gtk.FileChooserDialog(Gtk.FileChooserAction.OPEN)
+        dialog.set_transient_for(self)
+        dialog.set_title("Add tracks to your playlist")
+        dialog.add_button("_Open", Gtk.ResponseType.OK)
+        dialog.add_button("_Cancel", Gtk.ResponseType.CANCEL)
+        dialog.set_default_response(Gtk.ResponseType.OK)
+        response = dialog.run()
+        if response == Gtk.ResponseType.OK:
+            mixer.add(dialog.get_filename())
+            self.model.append(ModelItem(mixer.track_list[-1].metadata.track_name,\
+                              mixer.track_list[-1].duration))
+            self.show_all()
+        dialog.destroy()
+
+    def _start_skip(self, widget, value):
+        self.skipping = True
+
+    def _end_skip(self, widget, value):
+        mixer.skip(widget.get_value())
+        self.skipping = False
+
+    def show(self):
+        self.show_all()
+        self.playlistBox.hide()
+
+    def update(self, event, event_type):
         if event_type == mixer.PLAY_EVENT or event_type == mixer.NEXT_EVENT:
             self.play.get_children()[0].set_text(u'▌▌')
         elif event_type == mixer.PAUSE_EVENT or event_type == mixer.STOP_EVENT:
--- openmedia/player/track.py
+++ openmedia/player/track.py
@@ -5,25 +5,53 @@ import os
 class Track(object):
 
     def __init__(self, file_path):
-        self.file_path = os.path.expanduser(file_path)
+        self._file_path = os.path.expanduser(file_path)
         try:
-            self.track_file = open(self.file_path)
-            self.duration = float(self.track_file.seconds_length())
-            self.metadata = self.track_file.get_metadata()
+            track_file = open(self.file_path)
+            self._duration = float(track_file.seconds_length())
+            self._metadata = track_file.get_metadata()
         except UnsupportedFile:
             if file_path.endswith(".mp3"):
                 from mutagen.mp3 import MP3, MPEGInfo
                 from mutagen import File
-                self.track_file = File(self.file_path)
-                self.metadata = Metadata(self.track_file)
-                self.duration = self.track_file.info.length
+                track_file = File(self.file_path)
+                self._metadata = Metadata(track_file)
+                self._duration = track_file.info.length
             else:
-                self.duration = 0
-                self.metadata = None
+                self._duration = 0
+                self._metadata = None
+
+    @property
+    def name(self):
+        return self._metadata.track_name
+
+    @property
+    def file_path(self):
+        return self._file_path
+
+    @property
+    def metadata(self):
+        return self._metadata
+
+    @property
+    def duration(self):
+        return self._duration
 
 class Metadata(object):
 
     def __init__(self, mutFile):
-        self.artist_name = mutFile["TPE1"]
-        self.album_name = mutFile["TALB"]
-        self.track_name = mutFile["TIT2"]
+        self._artist_name = mutFile["TPE1"]
+        self._album_name = mutFile["TALB"]
+        self._track_name = mutFile["TIT2"]
+
+    @property
+    def artist_name(self):
+        return self._artist_name
+
+    @property
+    def album_name(self):
+        return self._album_name
+
+    @property
+    def track_name(self):
+        return self._track_name
