d023fe7149c9fbb97a1693334de6e3f0dd294290
Gabriela Alexandra Moldovan <gabi_250@live.com>
2017-01-25 21:45:59 +0000

    Replace button labels with icons.

--- openmedia/gui/controlbox.py
+++ openmedia/gui/controlbox.py
@@ -4,6 +4,8 @@ from gi.repository import Gtk
 from playlistbox import PlaylistBox
 from openmedia.observable.observable import Observer
 from openmedia.player import mixer
+from tools.iconhelp import get_button_image
+from tools.iconhelp import get_name
 
 
 class ControlBox(Gtk.VBox, Observer):
@@ -28,24 +30,30 @@ class ControlBox(Gtk.VBox, Observer):
         self.pack_start(self.button_box, False, False, 0)
 
     def _create_buttons(self):
-        self.play_button = Gtk.Button(label=u'▶')
+        self.play_button = Gtk.Button.new_from_icon_name(get_name("play"),
+                                                         Gtk.IconSize.BUTTON)
         self.play_button.connect("clicked", self._play_pause)
-        self.stop_button = Gtk.Button(label=u'■')
+        self.stop_button = Gtk.Button.new_from_icon_name(get_name("stop"),
+                                                         Gtk.IconSize.BUTTON)
         self.stop_button.connect("clicked", self._stop)
-        self.play_next_button = Gtk.Button(label=">>")
+        self.play_next_button = Gtk.Button.new_from_icon_name(get_name("skip"),
+                                                              Gtk.IconSize.BUTTON)
         self.play_next_button.connect("clicked", self._play_next)
         self.volume_button = Gtk.VolumeButton()
         self.volume_button.set_value(0.5)
         self.volume_button.connect("value-changed", self._volume)
         self.volume_button.x_align = 1.0
-        self.playlist_button = Gtk.ToggleButton(label="=")
+        self.playlist_button = Gtk.ToggleButton()
+        self.playlist_button.set_image(get_button_image(get_name("menu")))
         self.playlist_button.connect("clicked", self._toggle_playlist)
 
     def update(self, event, event_type):
         if event_type == mixer.PLAY_EVENT or event_type == mixer.NEXT_EVENT:
-            self.play_button.get_children()[0].set_text(u'▌▌')
+            image = get_button_image(get_name("pause"))
+            self.play_button.set_image(image)
         elif event_type == mixer.PAUSE_EVENT or event_type == mixer.STOP_EVENT:
-            self.play_button.get_children()[0].set_text('▶')
+            image = get_button_image(get_name("play"))
+            self.play_button.set_image(image)
         return False
 
     def show(self):
